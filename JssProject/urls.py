2fdd039ae193cfffd85519a5534116f860a8692d
svdoit <doemily@naver.com>
2020-09-10 01:58:23 +0900

    Delete function Added

--- JssProject/urls.py
+++ JssProject/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "index"),
     path('create/', views.create, name = "create"),
+    path('detail/<int:jss_id>', views.detail, name = "detail"),
+    path('delete/<int:jss_id>', views.delete, name = "delete"),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -15,3 +15,12 @@ def create(request):
             return redirect('index')
     jss_form = JssForm()
     return render(request, 'create.html', {'jss_form':jss_form})
+
+def detail(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    return render(request, 'detail.html', {'my_jss':my_jss})
+    
+def delete(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
